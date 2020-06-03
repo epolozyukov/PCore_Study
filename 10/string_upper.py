90d4de709e6c72be0007ea764a2159167c0faa81
Yevhen Poloziukov <epolozyukov@gmail.com>
2020-06-03 15:51:33 +0300

    task1

---
+++ 10/string_upper.py
@@ -0,0 +1,11 @@
+#Create a method is_uppercase() to see whether the string is ALL CAPS. For example:
+
+def is_uppercase(inp):
+    return inp.isupper()
+
+
+
+text = "ADASADA"
+text1 = "asdasaS"
+print(is_uppercase(text))
+print(is_uppercase(text1))
